696f3920c3292cf6ac3d7d4be246cebd90bdda83
momotheGOD <63006141+momotheGOD@users.noreply.github.com>
2024-03-31 11:43:32 +0330

    changed condition and humidity

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
+function refreshWeather(response) {
+	let temperatureElement = document.querySelector("#temperature");
+	let temperature = response.data.temperature.current;
+	temperatureElement.innerHTML = Math.round(temperature);
+	let cityElement = document.querySelector("#city");
+	cityElement.innerHTML = response.data.city;
+
+	let descriptionElement = document.querySelector("#description");
+	descriptionElement.innerHTML = response.data.condition.description;
+
+	let humidityElement = document.querySelector("#humidity");
+	humidityElement.innerHTML = response.data.temperature.humidity;
+	console.log(response.data);
+}
+
+function searchCity(city) {
+	//make api call and update the interface
+	let apiKey = "4974a3883e1ocea63dc4b19t8940f018";
+	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+	//api call
+	axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
 	event.preventDefault();
 	let searchInput = document.querySelector("#search-form-input");
 	let city = document.querySelector("#city");
-	city.innerHTML = searchInput.value;
+
+	searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form-element");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");

0b4e7795f3a24ffab2a9c445666587b30a94b53b
momotheGOD <63006141+momotheGOD@users.noreply.github.com>
2024-03-31 12:52:11 +0330

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -3,16 +3,37 @@ function refreshWeather(response) {
 	let temperature = response.data.temperature.current;
 	temperatureElement.innerHTML = Math.round(temperature);
 	let cityElement = document.querySelector("#city");
+	let speedElement = document.querySelector("#speed");
+	let date = new Date(response.data.time * 1000);
+	let timeElement = document.querySelector("#time");
+
 	cityElement.innerHTML = response.data.city;
 
 	let descriptionElement = document.querySelector("#description");
 	descriptionElement.innerHTML = response.data.condition.description;
 
 	let humidityElement = document.querySelector("#humidity");
-	humidityElement.innerHTML = response.data.temperature.humidity;
-	console.log(response.data);
+	humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+	speedElement.innerHTML = `${response.data.wind.speed}km/h`;
+	timeElement.innerHTML = formatDate(date);
 }
 
+function formatDate(date) {
+	let week = [
+		"Saturday",
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+	];
+	let day = week[date.getDay()];
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	return `${day} ${hours}:${minutes}`;
+}
 function searchCity(city) {
 	//make api call and update the interface
 	let apiKey = "4974a3883e1ocea63dc4b19t8940f018";
